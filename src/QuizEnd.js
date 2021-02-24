c22ba414149cd01a436643fd7a8d3f3d9f8ac7df
Milos Stojanovic <Milos.Stojanovic@flagstoneim.com>
2021-02-24 11:57:21 +0100

    Commit #9 MS

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 
 class QuizEnd extends Component {
+  handleResetClick() {
+    this.props.resetClickHandler();
+  }
+
   render() {
     return (
       <div>
         <p>Thanks for playing!</p>
-        <a href="">Reset Quiz</a>
+        <a onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
       </div>
-    )
+    );
   }
 }
 
-export default QuizEnd
+export default QuizEnd;
